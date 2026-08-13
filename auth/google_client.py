2e5be28e4f0a921f68f80923abca7eef7f6b5604
speedvan67 <iRcyJWKv2EQT51UAsYW1f2P5ZRg2@users.noreply.apostle.dev>
2026-08-13 14:03:45 +0000

    Optimize get_credentials with caching and type hints

--- auth/google_client.py
+++ auth/google_client.py
@@ -1,27 +1,22 @@
+from functools import lru_cache
+
 from google.oauth2.credentials import Credentials
 
 from auth.token_store import TOKEN_FILE
 
 
 SCOPES = [
-
     "https://www.googleapis.com/auth/gmail.send",
-
     "https://www.googleapis.com/auth/gmail.readonly",
-
     "https://www.googleapis.com/auth/calendar",
-
     "https://www.googleapis.com/auth/drive.file"
-
 ]
 
 
-def get_credentials():
-
+@lru_cache(maxsize=1)
+def get_credentials() -> Credentials:
+    """Load and cache Google OAuth credentials from the token file."""
     return Credentials.from_authorized_user_file(
-
         TOKEN_FILE,
-
         SCOPES
-
     )
